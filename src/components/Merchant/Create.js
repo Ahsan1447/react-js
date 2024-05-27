77c7c4e839706a05d80d4056b82806008d991544
Ahsan1447 <ahsanchaudary1447@gmail.com>
2024-05-27 17:50:57 +0500

    integrate delete, edit api, useEffect

--- src/components/Merchant/Create.js
+++ src/components/Merchant/Create.js
@@ -1,53 +1,107 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 function Create() {
   const navigate = useNavigate();
+  const [listMerchants, setMerchants ] = useState({
+      name: '',
+      email: '',
+      phone: ''
+  });
 
-  async function createMerchant() {
-    let name = document.querySelector('input[name="name"]').value;
-    let email = document.querySelector('input[name="email"]').value;
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.item?.id) {
+        setMerchants({
+            name: location.state.item.name,
+            email: location.state.item.email,
+            phone: location.state.item.phone
+        });
+    }
+  }, [location.state?.item?.id]);
+
+  const handlechange = (e) => {
+    const {name, value} = e.target;
+    setMerchants({
+      ...listMerchants,
+      [name]: value
+    });
+  }
+
+  const createMerchant = async(e) =>{
+    e.preventDefault();
 
     let local = localStorage.getItem("user-info");
     local = JSON.parse(local);
-
-    let data = {
+    let payload = {
       merchant: {
-        name: name,
-        email: email
+        name: listMerchants.name,
+        email: listMerchants.email,
+        phone: listMerchants.phone
       }
     };
+    try {
+      let api = location.state?.item?.id ? `http://localhost:3001/api/v1/merchants/${location.state.item.id}` : 'http://localhost:3001/api/v1/merchants';
 
-    await fetch("http://localhost:3001/api/v1/merchants", {
-      method: 'POST',
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": 'application/json',
-        "uid": local['uid'],
-        "client": local['client'],
-        "access-token": local['access_token']
-      },
-      body: JSON.stringify(data)
-    });
-    navigate('/login/merchant');
+      let method = location.state?.item?.id ? 'PUT' : 'POST';
+      const result = await fetch(api, {
+        method: method,
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": 'application/json',
+          "uid": local['uid'],
+          "client": local['client'],
+          "access-token": local['access_token']
+        },
+        body: JSON.stringify(payload)
+      });
+      if(result){
+        navigate('/login/merchant');
+      }
+
+
+    } catch (error) {
+      console.log({error});
+    }
   }
 
   return (
     <div>
-      <h1> Create </h1>
       <form>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text"
+          name="name"
+          value={listMerchants?.name}
+          onChange={handlechange}
+            >
+          </input>
         </label>
         <br />
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email"
+            name="email"
+           value={listMerchants?.email}
+            onChange={handlechange}>
+          </input>
         </label>
         <br />
-        <button type="submit" onClick={createMerchant}> Submit </button>
+        <label>
+          Phone:
+          <input type="text"
+            name="phone"
+            value={listMerchants?.phone}
+            onChange={handlechange}>
+          </input>
+        </label>
+        <br />
+        <div className='flex flex-col items-center'>
+          <button type="submit" onClick={createMerchant}> {location.state?.item?.id ? "Update" : "Create"} </button>
+        </div>
       </form>
     </div>
   );
--- src/components/Merchant/Index.js
+++ src/components/Merchant/Index.js
@@ -1,28 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Index() {
-  const [listMerchants, setMerchants ] = useState([{}]);
-  let data = localStorage.getItem("user-info");
+  const [listMerchants, setListMerchants ] = useState(null);
+  const [updateMerchant, setUpdateMerchant] = useState(null);
 
-  data = JSON.parse(data);
+  useEffect(() => {
+    getMerchants();
+  }, []);
 
-  async function getMerchants() {
-    let result = await fetch("http://localhost:3001/api/v1/merchants", {
-      method: 'GET',
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": 'application/json',
-        "uid": data['uid'],
-        "client": data['client'],
-        "access-token": data['access_token']
+  const getMerchants = async () => {
+    try {
+      let data = localStorage.getItem("user-info");
+      data = JSON.parse(data);
+      let result = await fetch("http://localhost:3001/api/v1/merchants", {
+        method: 'GET',
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": 'application/json',
+          "uid": data['uid'],
+          "client": data['client'],
+          "access-token": data['access_token']
+        }
+      });
+
+      result = await result.json();
+      setListMerchants(result?.data);
+
+    } catch (error) {
+      console.log({error});
+    }
+  }
+
+  const handlDelete = async (id) => {
+    let local = localStorage.getItem("user-info");
+    local = JSON.parse(local);
+
+    try {
+      let result = await fetch(`http://localhost:3001/api/v1/merchants/${id}`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": 'application/json',
+          "uid": local['uid'],
+          "client": local['client'],
+          "access-token": local['access_token']
+        }
+      });
+      if(result){
+        getMerchants();
       }
-    });
-
-    result = await result.json();
-    let result_data = result['data'];
-    setMerchants(result_data);
-    console.log(listMerchants);
+    }
+    catch (error) {
+      console.log({error});
+    }
+    console.log(id);
   }
 
   return (
@@ -32,39 +64,37 @@ function Index() {
       </div>
 
       <div>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th className='px-9'>Id</th>
-              <th className='px-9'>Name</th>
-              <th className='px-9'>Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            listMerchants.map((item) => (
-              <tr key={`${item.id}`} >
-                <td className='px-9'>{item.id}</td>
-                <td className='px-20'>{item.name}</td>
-                <td className='px-9'>{item.phone}</td>
+        {listMerchants?.length > 0 ? (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th className='px-9'>Id</th>
+                <th className='px-9'>Name</th>
+                <th className='px-9'>Phone</th>
+                <th className='px-9'>action</th>
               </tr>
-            ))
-          }
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {listMerchants?.map((item) => (
+                <tr key={`${item?.id}`} >
+                  <td className='px-9'>{item?.id}</td>
+                  <td className='px-20'>{item?.name}</td>
+                  <td className='px-9'>{item?.phone}</td>
+                  <td className='px-9' ><a href='#'onClick={()=>handlDelete(item?.id)}>Delete</a>  <Link to='/login/merchant/create' state={{item: item}} >Edit</Link></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
 
-      <div>
-        <button className="btn btn-primary" onClick={getMerchants}
-          >Get Merchants
-          </button>
-      </div>
       <br />
       <div className='flex flex-col items-center'>
-          <Link to="/login/merchant/create">CreateMerchant</Link>
+        <Link to="/login/merchant/create">Create Merchant</Link>
       </div>
     </>
-
   );
 }
 
